ccce5916b673dfe067ace0d93d8636d8fd53be33
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-08-02 15:24:12 +0200

    first commit

---
+++ js/scipt.js
@@ -0,0 +1,32 @@
+/* TRACCIA
+
+MILESTONE 1
+Scrivere un programma che stampi in console i numeri da 1 a 100, ma che
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+MILESTONE 2:
+Creiamo un elemento che faccia da contenitore nel DOM e poi riempiamolo con i degli elementi via JS.
+Possiamo usare varie tecniche  (template literals, innerHTML, appendecc)
+
+MILESTONE 3:
+Applichiamo stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+Come abbiamo visto ci sono usare varie tecniche (style , className, classList)
+Se siete a corto di idee per lo stile, potreste prendere spunto dallo screenshot fornito in consegna.
+
+
+*/
+
+/* LA MIA SCALETTA PER PROCEDERE*/
+
+
+// FASE DI PREPARAZIONE
+
+// FASE DI RACCOLTA DATI
+
+// FASE DI VALIDAZIONE
+
+// FASE DI LAVORAZIONE DATI
+
+// FASE DI OUTPUT
